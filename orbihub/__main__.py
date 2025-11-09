e60dc2e7b92c6b5c5a130d7828a7c8c1ef2f5f99
Eriel Cabrera <erielcabrera1234@gmail.com>
2025-11-08 20:03:10 -0500

    created the templates that apps will be created with when added to the marketplace

--- orbihub/__main__.py
+++ orbihub/__main__.py
@@ -1,1 +1,12 @@
-#
+#!/usr/bin/env python3
+"""
+Calculator Application Main Entry Point
+"""
+
+# from .frontend import main  # Ensure 'frontend' is the correct module where 'main' is defined
+
+if __name__ == '__main__':
+    # main()  # Call the main function to start the application
+from PyQt6.QtWidgets import QMainWindow ,QWidget
+import sys
+

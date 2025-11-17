793aab358a13fba21ebe454fcdaaffc254993148
Eriel Cabrera <erielcabrera1234@gmail.com>
2025-11-17 11:32:10 -0500

    created a template to just fix image/widgets popping up in scroll areag

--- orbihub/ui/apps_template/apps_frontend.py
+++ orbihub/ui/apps_template/apps_frontend.py
@@ -1,5 +1,7 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout
+from PyQt6.QtGui import QPixmap
 from orbihub.ui.apps_template.ui_app_template import Ui_app_template_format
+from orbihub.utils.logger import logger
 
 class form_apps(QWidget, Ui_app_template_format):
   """
@@ -7,8 +9,18 @@ class form_apps(QWidget, Ui_app_template_format):
   
   Class sets up the Ui for the home page and initializes the components defined in the Ui file
   """
-  def __init__(self):
+  def __init__(self, app_name : str, image_path : str, version : str):
     """sets up the ui file to display in the application"""
     super().__init__()
-
     self.setupUi(self)
+    image_label = QLabel(parent=self.app_image)
+    image_label.setScaledContents(True)
+    image_label.setGeometry(self.app_image.rect())
+    pixmap = QPixmap(image_path)
+    if pixmap.isNull():
+        logger.error(f"Failed to load image: {image_path}")
+    else:
+        logger.info(f"Image loaded successfully: {pixmap.size()}")
+    image_label.setPixmap(pixmap) 
+
+    #TODO: Add a layout to make the label fill the frame & fix image
